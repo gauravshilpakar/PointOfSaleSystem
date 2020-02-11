49a33aaf9d8f266037b08c77c44a6a716f475943
Pranesh Shrestha <praneshshrestha10@gmail.com>
2020-02-10 19:34:20 -0600

    feb10

--- src/app/App.java
+++ src/app/App.java
@@ -3,7 +3,14 @@ package app;
 import java.util.*;
 
 public class App {
+
+    static Map<String, data> inventory = new HashMap<>();
+
     public static void main(String[] args) {
         // inventory.displayInventory();
     }
+}
+
+class data {
+    int price, count;
 }
