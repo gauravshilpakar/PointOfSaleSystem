50125e74c29de8786b49c5967a109968be22f072
gauravshilpakar <gaurav.shilpakar@gmail.com>
2020-02-10 18:43:41 -0600

    new

--- src/app/App.java
+++ src/app/App.java
@@ -1,10 +1,16 @@
 package app;
 
-import java.util.*;
+// import java.util.*;
 
 public class App {
     public static void main(String[] args) {
-        AddInventory inventory = new AddInventory("Coke", 10);
+        Inventory inventory = new Inventory();
+
+        inventory.AddInventory("Coke", 10);
+        inventory.AddInventory("Fanta", 10);
+        inventory.AddInventory("Choc", 10);
+        inventory.AddInventory("Test", 10);
+        inventory.AddInventory("Coke", 10);
         inventory.displayInventory();
     }
 }
--- src/app/Inventory.java
+++ src/app/Inventory.java
@@ -2,14 +2,13 @@ package app;
 
 import java.util.*;
 
-class AddInventory {
+class Inventory {
     private Map<String, Integer> inventory = new HashMap<>();
 
-    AddInventory(String item, int amount) {
+    void AddInventory(String item, int amount) {
         if (inventory.containsKey(item)) {
             int stock = inventory.get(item);
             inventory.put(item, stock + amount);
-
             int newstock = inventory.get(item);
             System.out.println("New Stock of " + item + ": " + newstock);
         } else {
@@ -19,10 +18,8 @@ class AddInventory {
     }
 
     void displayInventory() {
-        Set<Map.Entry<String, Integer>> dI = inventory.entrySet();
-        for (Map.Entry<String, Integer> ite : dI) {
-            System.out.println(ite.getValue());
-        }
+        System.out.println("\nItem\t\tValue");
+        for (Map.Entry<String, Integer> entry : inventory.entrySet())
+            System.out.println(entry.getKey() + "\t\t" + entry.getValue());
     }
-
 }
