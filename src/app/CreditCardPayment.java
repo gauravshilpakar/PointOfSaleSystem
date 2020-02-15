76bdd97d02f9b438eab93f02b1025cef4d06776f
Sudesh subedi <47485180+ssubedi123@users.noreply.github.com>
2020-02-14 23:01:22 -0600

    Add files via upload

---
+++ src/app/CreditCardPayment.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author binda
+ */
+public class CreditCardPayment extends Payment {
+
+    public CreditCardPayment(double d, String fred, String string, String string0) {
+    }
+
+    CreditCardPayment(double d, String raju, String string, String string0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
